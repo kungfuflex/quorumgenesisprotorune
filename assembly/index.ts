19a7104261787d6c5bf395d87e92f58a9e7fd5f8
sandshrewmetadev <inquiries@sandshrew.io>
2024-06-29 18:23:49 +0000

    widebst

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,7 +1,5 @@
-import { Box } from "metashrew-as/assembly/utils/box";
 import { _flush, input, get, set } from "metashrew-as/assembly/indexer/index";
 import { IndexPointer } from "metashrew-as/assembly/indexer/tables";
-import { intoString, scriptParse } from "metashrew-as/assembly/utils/yabsp";
 import { Block } from "metashrew-as/assembly/blockdata/block";
 import { parsePrimitive } from "metashrew-as/assembly/utils/utils";
 import {
@@ -11,17 +9,36 @@ import {
   OutPoint,
 } from "metashrew-as/assembly/blockdata/transaction";
 import { console } from "metashrew-as/assembly/utils/logging";
-import { toRLP, RLPItem } from "metashrew-as/assembly/utils/rlp";
-import { BST } from "metashrew-as/assembly/indexer/bst";
-import { readULEB128ToU128 } from "./leb128";
 import { u256, u128 } from "as-bignum/assembly";
-import { Index } from "./indexer";
-import { GENESIS } from "./indexer/constants";
-import { Index as SpendablesIndex } from "metashrew-spendables/assembly/indexer";
+import { quorum as protobuf } from "./proto/quorum";
+import { BSTU128 } from "./quorum/widebst";
+import { u128ToHex } from "./utils";
 
-export function trap(): void {
-  unreachable();
+const QUORUM_GENESIS_PROTORUNE_HEIGHT = 849236;
+const QUORUM_GENESIS_PROTORUNE_TXINDEX = 298;
+/*
+
+class QuorumMessage extends ProtoMessage {
+  handle(context: MessageContext): boolean {
+    const message = protobuf.QuorumProtoMessage.decode(context.calldata);
+    message.data.
+    const i = context.runes.findIndex((v: IncomingRune, i: i32, ary: Array<IncomingRune>) => v.runeId.height === 849236 && v.runeId.txindex === 298);
+    i
+  }
 }
+*/
+
+export function test_BSTU128(): void {
+  const bst = BSTU128.at(IndexPointer.for("/test"));
+  u128.from(0x100);
+  bst.set(u128.from(0x100), String.UTF8.encode("test"));
+  bst.set(u128.from(0x140), String.UTF8.encode("test"));
+  bst.set(u128.from(0x104), String.UTF8.encode("test"));
+  console.log(u128ToHex(bst.seekGreater(u128.from(0x103))));
+  bst.set(u128.from(0x104), new ArrayBuffer(0));
+  console.log(u128ToHex(bst.seekGreater(u128.from(0x103))));
+}
+/*
 
 export function _start(): void {
   const data = input();
@@ -33,9 +50,10 @@ export function _start(): void {
   }
   const block = new Block(box);
   if (height >= GENESIS) SpendablesIndex.indexBlock(height, block);
-  Index.indexBlock(height, block);
+  Protorune.indexBlock<QuorumMessage>(height, block);
   _flush();
 }
+*/
 
 export * from "./tests";
 export * from "./view";
--- tests/release.spec.ts
+++ tests/release.spec.ts
@@ -5,33 +5,11 @@ import { IndexerProgram } from "metashrew-test";
 import * as path from "node:path";
 
 const log = (obj: any) => {
-  console.log(inspect(obj, false, 10, true));
+  console.log(obj.substr(0, obj.length - 1));
 };
 
-// const runTest = (s) =>
-//   it(s, async () => {
-//     const program = new IndexerProgram(
-//       new Uint8Array(
-//         Array.from(
-//           fs.readFileSync(path.join(__dirname, "..", "build", "debug.wasm")),
-//         ),
-//       ).buffer,
-//     );
-//     program.on("log", log);
-//     await program.run(s);
-//     await new Promise((r) => setTimeout(r, 2000));
-//     return program;
-//   });
-describe("metashrew-runes", () => {
-  // [
-  //   "test_indexEtching",
-  //   "test_genesisTransaction",
-  //   "test_secondTransaction",
-  //   "test_oneFortyEight",
-  //   "test_fifteen",
-  // ].map((v) => runTest(v));
-
-  it("should check runes witness script", async () => {
+const runTest = (s) =>
+  it(s, async () => {
     const program = new IndexerProgram(
       new Uint8Array(
         Array.from(
@@ -39,43 +17,13 @@ describe("metashrew-runes", () => {
         ),
       ).buffer,
     );
-    program.on("log", console.log);
-    program.setBlock(
-      fs.readFileSync(path.join(__dirname, "849236.hex"), "utf8"),
-    );
-    program.setBlockHeight(849236);
-    await program.run("testCommitment");
+    program.on("log", log);
+    await program.run(s);
+    await new Promise((r) => setTimeout(r, 2000));
     return program;
   });
-  // it("should check name things", async () => {
-  //   const program = new IndexerProgram(
-  //     new Uint8Array(
-  //       Array.from(
-  //         fs.readFileSync(path.join(__dirname, "..", "build", "debug.wasm")),
-  //       ),
-  //     ).buffer,
-  //   );
-  //   program.on("log", console.log);
-  //   program.setBlock(
-  //     fs.readFileSync(path.join(__dirname, "849236.hex"), "utf8"),
-  //   );
-  //   program.setBlockHeight(849236);
-  //   await program.run("testFieldToName");
-  //   let spaced_name = "UNCOMMONGOODS";
-  //   const spacer = "•";
-  //   const spacers = 128;
-  //   const bitField = spacers.toString(2);
-  //   let x = 0;
-  //   bitField
-  //     .split("")
-  //     .reverse()
-  //     .map((d, i) => {
-  //       if (d == "1") {
-  //         spaced_name = `${spaced_name.slice(0, i + 1 + x)}${spacer}${spaced_name.slice(i + 1 + x)}`;
-  //         x++;
-  //       }
-  //     });
-  //   console.log(spaced_name);
-  //   return program;
-  // });
+describe("QUORUM•GENESIS•PROTORUNE", () => {
+  [
+    "test_BSTU128"
+  ].map((v) => runTest(v));
 });
